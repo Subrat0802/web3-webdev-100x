f66419fc5fa83ed9c9415d1bda0af17b501b2ec2
Subrat Mishra <piyush.subrat8@gmail.com>
2025-02-02 21:47:04 +0530

    next week-19

---
+++ webdev/week-19-nextjs/app/auth/signin/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Signin(){
+    return <div>
+        Sign In Page 
+        <input placeholder="Email"/>
+        <input placeholder="Email"/>
+        <button>Signin</button>
+    </div>
+}
