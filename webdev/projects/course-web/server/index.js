fd62815ac007b4ebce77dddb47e29fc12c787c83
Subrat Mishra <piyush.subrat8@gmail.com>
2025-02-13 18:17:00 +0530

    db connected

--- webdev/projects/course-web/server/index.js
+++ webdev/projects/course-web/server/index.js
@@ -1,12 +1,18 @@
 const express = require("express");
+require("dotenv").config();
+const {connect} = require("./config/db");
 
 const app = express();
+const PORT = process.env.PORT || 3001
+
 
 app.use(express.json());
 
-app.use("/api/v1/user");
-app.use("/api/v1/profile")
 
-app.listen(3000, () => {
+// app.use("/api/v1/user");
+// app.use("/api/v1/profile")
+connect();
+
+app.listen(PORT, () => {
     console.log("App is running");
 })
