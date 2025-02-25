d1609d22b42491a6c995d76c5715831c54ce6bfd
Subrat Mishra <piyush.subrat8@gmail.com>
2025-02-26 01:09:14 +0530

    custom button ui

--- webdev/projects/brainly-again/frontend/src/App.tsx
+++ webdev/projects/brainly-again/frontend/src/App.tsx
@@ -1,13 +1,17 @@
-
+import {PlusIcon} from "./icons/Plusicon";
 import './App.css'
 import Button from './components/ui/Button'
+import { Shareicon } from "./icons/Shareicon";
 
 function App() {
 
 
   return (
     <>
-      <Button varient={'primary'} size={'lg'} text={"Share Brainly"} />
+      <div className='flex gap-4 m-2'>
+        <Button startIcon={<PlusIcon size={'md'}/>} varient={'secondary'} size={'md'} text={'Add Brainly'} />
+        <Button startIcon={<Shareicon size={'md'}/>} varient={'primary'} size={'md'} text={"Share Brainly"} />
+      </div>
     </>
   )
 }
--- webdev/projects/brainly-again/frontend/src/components/ui/Button.tsx
+++ webdev/projects/brainly-again/frontend/src/components/ui/Button.tsx
@@ -1,19 +1,36 @@
 
+
+// type varients = "primary" | "secondary"
 export interface ButtonProps {
     varient: "primary" | "secondary",
-    size:"sm" | "md" | "lg",
+    size: "sm" | "md" | "lg",
     text: string,
-    startIcon?: any,   // #?- means this optional
-    endIcon?: any,      // #?- means this optional
+    startIcon?: any,   // #?- means this is optional
+    endIcon?: any,      // #?- means this is optional
     onClick?: () => void,
 }
 
-const Button = (props: ButtonProps) => {
-  return (
-    <div>
-        <button>{props.text}</button>
-    </div>
-  )
+const varientStyle = {
+    "primary": "bg-purple-600 text-white hover:bg-purple-500 hover:text-white transition-all duration-200",
+    "secondary": "bg-purple-200 text-purple-600 hover:bg-purple-500 hover:text-white transition-all duration-200"
+}
+
+const sizeStyle = {
+    "sm": "py-2 px-4",
+    "md": "py-3 px-6",
+    "lg": "py-4 px-7"
+}
+
+const defaultStyle = "rounded-lg p-4 flex gap-2 justify-center items-center hover:scale-95"
+
+const Button = ({ varient, size, text, startIcon, endIcon, onClick }: ButtonProps) => {
+    return (
+        <div>
+            <button className={` ${varientStyle[varient]} ${defaultStyle} ${sizeStyle[size]}`}>
+                {startIcon ? <div>{startIcon}</div> : null} {text} {endIcon ? <div>{endIcon}</div> : null}
+            </button>
+        </div>
+    )
 }
 
 export default Button
