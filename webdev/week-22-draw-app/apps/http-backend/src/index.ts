d37c94357d49a3d3aaff843900945ba79e326443
subrat0802 <piyush.subrat8@gmail.com>
2025-03-14 21:43:22 +0530

    draw-app-day-1

---
+++ webdev/week-22-draw-app/apps/http-backend/src/index.ts
@@ -0,0 +1,53 @@
+import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import {JWT_SECRET} from "@repo/backend-common/config"
+import { middleware } from "./middleware";
+import {CreateUserSchema, SignInSchema, CreateRoomSchema} from "@repo/common/types";
+
+const app = express();
+
+app.post("/signup", (req:Request, res:Response) => {
+    const data = CreateUserSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message:"Incorrect Inputs"
+        })
+        return;
+    }
+    res.json({
+        userId:123
+    })
+})
+
+app.post("/signin", (req, res) => {  
+    const data = SignInSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message:"Incorrect Inputs"
+        })
+        return;
+    }
+    const userId = 1;
+    const token = jwt.sign({
+        userId,
+    }, JWT_SECRET);
+
+    res.json({
+        token
+    })
+})
+
+app.post("/room", middleware, (req, res) => {
+    const data = CreateRoomSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message:"Incorrect Inputs"
+        })
+        return;
+    }
+    res.json({
+        roomId:123
+    })
+})
+
+app.listen(3001);
---
+++ webdev/week-22-draw-app/apps/http-backend/src/middleware.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Response, Request } from "express";
+import jwt from "jsonwebtoken";
+import {JWT_SECRET} from "@repo/backend-common/config"
+
+export function middleware(req: Request, res:Response, next:NextFunction){
+    const token = req.headers["authorization"] ?? "";
+
+    const decode = jwt.verify(token, JWT_SECRET);
+
+    if(decode){
+        //@ts-ignore
+        req.userId = decode.userId;
+        next();
+    }else{
+        res.status(403).json({
+            message:"unauthorized"
+        })
+    }
+}
