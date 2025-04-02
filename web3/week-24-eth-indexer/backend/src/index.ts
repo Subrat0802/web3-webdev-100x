8117be95d341e99d859ada1bcd3bb088fe02a4c5
subrat0802 <piyush.subrat8@gmail.com>
2025-04-02 23:00:34 +0530

    initialize app

---
+++ web3/week-24-eth-indexer/backend/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import pg from "pg";
+
+const app = express();
+
+app.post("/signup", (req, res) => {
+
+})
+
+app.get("/depositeAddress/:userId", (req, res) => {
+    
+})
+
+app.listen(3000);
