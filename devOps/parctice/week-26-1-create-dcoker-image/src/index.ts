c577be8280a4f52129ec88b5e6f2b21112920ffc
subrat0802 <piyush.subrat8@gmail.com>
2025-08-06 22:35:51 +0530

    docker parctice part one

---
+++ devOps/parctice/week-26-1-create-dcoker-image/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hi there docker file is started.");
+})
+
+app.post("/user", (req, res) => {
+    const {username, password} = req.body;
+
+
+})
+
+app.listen(3000);
+console.log("ENV VARIABLE", process.env.DATABASE_URL)
