46bbd42c47075cb48a49a0fa038df34711b42f30
subrat0802 <piyush.subrat8@gmail.com>
2025-08-08 01:05:26 +0530

    docker.2

--- devOps/parctice/week-26-2-create-just-mongo-img/src/dbconnect.ts
+++ devOps/parctice/week-26-2-create-just-mongo-img/src/dbconnect.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://localhost:27017/without_vol")
+mongoose.connect("mongodb://mongoagain:27017/without_vol")
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
